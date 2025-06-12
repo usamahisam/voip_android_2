9eefad231c8303258f46ab940c641cfccaacfa43
usamahisam <hisamusama@gmail.com>
2025-06-12 15:59:19 +0700

    feat: voicemail

--- src/main/java/com/breakreasi/voip_android_2/sip/SipService.kt
+++ src/main/java/com/breakreasi/voip_android_2/sip/SipService.kt
@@ -130,7 +130,6 @@ class SipService : Service() {
                 it?.delete()
                 sipAccount!!.call = null
             }
-            sipAccount!!.destination = ""
         } catch (_: Exception) {
         }
     }
